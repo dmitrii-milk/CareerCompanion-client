d09e90a37949c252ad0d675dba47094808d3e098
kiba <kiba@quartsoft.com>
2023-10-17 23:17:40 +0300

    add prettier and eslint

---
+++ src/constants/env.ts
@@ -0,0 +1,5 @@
+export const ENV = {
+    port: import.meta.env.VITE_PORT,
+    environment: import.meta.env.VITE_ENV
+};
+
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+export interface ImportMetaEnv {
+    readonly VITE_ENV: string
+    readonly VITE_PORT: string
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
